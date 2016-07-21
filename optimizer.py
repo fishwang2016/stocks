68974aaa053e2ca6da1afbb18a2e4725db03ccb3
Fish <zhenyu2004@hotmail.com>
2016-07-21 22:08:55 +0800

    added optimizer.py
    initial files.

---
+++ optimizer.py
@@ -0,0 +1,32 @@
+"""
+Find minimum values of  functions
+ Build parameterized models based on data
+ Refine allocations to stocks in profolio
+
+"""
+
+"""Minimize on objective function,using Scipy"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as spo
+
+def f(X):
+    """Given a scalar x, return some value.(a real number)"""
+
+    Y = (X-1.5)**2 +0.5
+
+    print "X={}, Y={}.".format(X,Y) # for tracing
+
+    return Y
+def test_run():
+    Xguess=2.0
+    min_result=spo.minimize(f,Xguess,method='SLSQP',options={'disp':True})
+    print "Minima found at:"
+    print "X = {}, Y = {}".format(min_result.x,min_result.fun)
+
+if __name__=='__name__':
+    test_run()
+
+    print "ok"
