e4f648f82089fe40d47f83bef0a93931a25d961a
fishwang2016 <zhenyu2004@hotmail.com>
2016-07-21 21:28:40 +0800

    daily

--- daily_returns.py
+++ daily_returns.py
@@ -9,11 +9,13 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
+import numpy as np
+
 from utility_stock import get_data ,plot_data,get_daily_returns
 
 def test_run():
     
-    symbols =["GOOG",'IBM','AAPL']
+    symbols =['IBM','GOOG','AAPL']
     
     start_date = '2015-01-01'
     end_date = '2016-12-31'
@@ -23,7 +25,33 @@ def test_run():
     
     daily_returns= get_daily_returns(df)
     
-    daily_returns['SPY'].hist(bins=50,figsize=(15,5))
+    daily_returns['SPY'].hist(bins=20,figsize=(15,5),label='SPY')
+    daily_returns['IBM'].hist(bins=20,label='IBM')
+    plt.legend(loc="upper left")
+    
+    mean = daily_returns['SPY'].mean()
+    print "Mean: ",mean
+    std = daily_returns['SPY'].std()
+    print "Std: ", std
+    
+    print daily_returns['SPY'].kurtosis()
+    
+    print daily_returns.corr(method='pearson')
+    
+    plt.axvline(mean,color='w',linestyle='dashed',linewidth=2)
+    plt.axvline(std,color ='r',linestyle='dashed',linewidth=2)
+    plt.axvline(-std,color ='r',linestyle='dashed',linewidth=2)
+    
+    
+    daily_returns.plot(kind='scatter',x='SPY',y='IBM',figsize=(15,5))   
+    
+    # y = ax+ b
+    
+    beta_IBM , alpha_IBM = np.polyfit(daily_returns['SPY'],daily_returns['IBM'],1)
+    plt.plot(daily_returns['SPY'],beta_IBM*daily_returns['SPY']+alpha_IBM,'-',color='r')
+    
+    plt.show()
+
    
     
     #plot_data(daily_returns['SPY'])
