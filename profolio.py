43c558047170a39169eaed7a87d884eaf1bf1c63
fishwang2016 <zhenyu2004@hotmail.com>
2016-07-22 10:37:54 +0800

    added initial file
    This file is to add a function that can generate an optimized profolio
    given an input base on  history data.

---
+++ profolio.py
@@ -0,0 +1,12 @@
+"""
+# Input some stocks and findout optimized profolio
+
+Example:
+
+stocks = ["GOOG","IBM",AAPL]
+ratio  =[0.2,0.3,0.5]
+
+output an optimized ration according to the history data
+
+"""
+
