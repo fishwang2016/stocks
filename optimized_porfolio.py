fe7d637145783c443926af54365991fc6c5a9c8c
Fish <zhenyu2004@hotmail.com>
2016-07-23 13:47:37 +0800

    sharp ratio calculation

--- optimized_porfolio.py
+++ optimized_porfolio.py
@@ -22,11 +22,19 @@ How to evaluate portfolio performance:
     rp: return of the portfolio
     rf: risk-free rate
     THETAp: standard deviation of returns of the portfolio 
-
+   
 """
 from utility_stock import get_data,plot_data,get_daily_returns
 import pandas as pd
 import matplotlib.pyplot as plt
+import math 
+
+def sharp_ratio(returns,rr =0.03):
+    rr = math.pow(rr,1.0/252.0)
+    rp = returns.sum()
+    std = returns.std()
+    
+    return (rp -rr)/std
 
 def normalized_prices(data):
     """
@@ -44,33 +52,34 @@ def portfolio_returns(data,weight):
     # daily returns for each stock
     daily = get_daily_returns(data)
     
-    print "data"
-    print daily
-    print "_________"
+   # print "data"
+   # print daily
+   # print "_________"
     p_daily = daily * weight
        
     p_daily["sum"] = p_daily.sum(axis = 1)    
     
-    print p_daily
+    #print p_daily
     return p_daily 
 
 
 def test_run():
-    weight =[0.2,0.5,0.3]
+    weight =[0.0,0.5,0.3]
     symbols = ["IBM","GOOG","AAPL"]
     start_date ='2015-01-01'
-    end_date ='2015-1-31'
+    end_date ='2015-12-31'
     dates = pd.date_range(start_date, end_date)
     data = get_data(symbols, dates)
     #print data
     data = normalized_prices(data)
     #print data.iloc[0]
     #daily = get_daily_returns(data)
-    print "symbols ",symbols
-    print data[symbols]
-    print "end symbols"
+    #print "symbols ",symbols
+    #print data[symbols]
+    #print "end symbols"
     p_daily = portfolio_returns(data[["IBM","GOOG","AAPL"]],weight)
     #print p_daily
+    print sharp_ratio(p_daily["sum"])
     data.plot(title = 'Normalized Price',figsize=(15,5))
     plt.legend(loc ='upper left')
     plt.show()
